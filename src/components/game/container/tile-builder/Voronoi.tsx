62a9e5b67f3eed36e2819ba8a45c91ae11384ad1
Barry Niehuser <bniehuser@gmail.com>
2021-03-23 21:54:24 -0700

    maybe directed line?

--- src/components/game/container/tile-builder/Voronoi.tsx
+++ src/components/game/container/tile-builder/Voronoi.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Diagram, Voronoi } from 'voronoijs';
+import { Diagram, Edge, Voronoi } from 'voronoijs';
 import { generatePoints } from '../../../../util/geometry/poisson';
 
 type Vec2 = { x: number, y: number };
@@ -14,6 +14,10 @@ const sameLine = (a: { va: Vec2, vb: Vec2 }, b: { va: Vec2, vb: Vec2 }) => {
 
 const lineSlope = (b: Vec2, e: Vec2) => {
   return (e.y - b.y) / (e.x - b.x);
+};
+
+const edgeSlope = (e: Edge) => {
+  return (e.vb.y - e.va.y) / (e.vb.x - e.va.x);
 };
 
 const drawVoronoi = (ctx: CanvasRenderingContext2D, diagram: Diagram) => {
@@ -32,6 +36,26 @@ const drawVoronoi = (ctx: CanvasRenderingContext2D, diagram: Diagram) => {
     ctx.lineWidth = 2;
     ctx.beginPath();
     // find yer lines
+    // line left->right
+    const sx = Math.random() * 712 - 100;
+    const ex = Math.random() * 712 - 100;
+    const xSlope = 512 / (ex - sx);
+    let e: undefined|Edge = diagram.edges
+      .filter(e => e.va.y === 0)
+      .reduce((a, c) => Math.abs(sx - c.va.x) <= Math.abs(sx - a.va.x) && Math.abs(xSlope - edgeSlope(c)) < Math.abs(xSlope - edgeSlope(c)) ? c : a);
+    let tries = 0;
+    while (e !== undefined && tries < 10000) {
+      ctx.moveTo(e.va.x, e.va.y);
+      ctx.lineTo(e.vb.x, e.vb.y);
+      const olde: Edge = e;
+      const edges = diagram.edges
+        .filter(e => e.va.x === olde.vb.x && e.va.y === olde.vb.y);
+      e = edges ? edges.reduce((a, c) => c.vb.y > olde.vb.y && Math.abs(xSlope - edgeSlope(c)) < Math.abs(xSlope - edgeSlope(c)) ? c : a) : undefined;
+      tries++;
+      if (tries === 1000) {
+        console.error(e);
+      }
+    }
     ctx.stroke();
   }
 };
